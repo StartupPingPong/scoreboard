902f3ca4b585a307e7bb2aaf4c7918948f9973bf
joseph_ctu <sephxd1234@gmail.com>
2018-01-18 21:43:34 +0100

    feat: introduce GET teams data route

---
+++ routes/teams.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET teams listing. */
+router.get("/", function(req, res, next) {
+  res.json([
+    {
+      id: 1,
+      teamName: "SUPP"
+    },
+    {
+      id: 2,
+      teamName: "CSE"
+    }
+  ]);
+});
+
+module.exports = router;
