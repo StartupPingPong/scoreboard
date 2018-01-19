1fb76e2bcd41b5f5771f434dc2f0ca6dbc93e9d0
joseph_ctu <sephxd1234@gmail.com>
2018-01-19 20:36:52 +0100

    test: verify that teams are rendered when passed as props

--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { shallow } from "enzyme";
 
-import App from "./App";
+import { App } from "./App";
 import { strings } from "../constants";
 
 describe("App", () => {
-  let app = shallow(<App />);
+  let props = {
+    teams: [
+      {
+        id: 0,
+        teamName: "test me"
+      }
+    ]
+  };
+
+  let app = shallow(<App {...props} />);
 
   it("renders properly", () => {
     expect(app).toMatchSnapshot();
@@ -15,5 +24,9 @@ describe("App", () => {
   it("displays `SUPP` title", () => {
     expect(title.exists()).toBe(true);
     expect(title.text()).toEqual(strings.eventName);
+  });
+
+  it("renders the teams given as props", () => {
+    expect(app.find(".lead").text()).toEqual("test me");
   });
 });
