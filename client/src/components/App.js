bb3726f2a4652673c34a45ca23ce978fba5275e6
joseph_ctu <sephxd1234@gmail.com>
2018-01-19 20:36:29 +0100

    feat: connect to redux and render teams

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { strings } from "../constants";
+import * as actions from "../actions";
 
-class App extends Component {
+export class App extends Component {
+  componentDidMount() {
+    this.props.dispatch({ type: actions.GET_ALL_TEAMS });
+  }
+
   render() {
+    const { teams } = this.props;
     return (
       <div>
         <h1 className="main-title display-1">{strings.eventName}</h1>
+        {teams.map(team => (
+          <p className="lead" key={team.id}>
+            {team.teamName}
+          </p>
+        ))}
       </div>
     );
   }
 }
 
-export default App;
+export default connect(state => ({ teams: state.teams }), null)(App);
