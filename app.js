b930b8c00f38c70020bb34a7038202a4671b43cd
joseph_ctu <sephxd1234@gmail.com>
2018-01-20 19:27:18 +0100

    feat: add use to serve static build, and ejs

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ var teams = require("./routes/teams");
 var app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+app.use(express.static(path.join(__dirname, "client/build")));
+app.set("view engine", "ejs");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
