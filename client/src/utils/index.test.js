4c1f7781ab0924af77b551c80b84f09fb328e321
joseph_ctu <sephxd1234@gmail.com>
2018-01-21 08:50:36 +0100

    refactor: made tests more readable

--- client/src/utils/index.test.js
+++ client/src/utils/index.test.js
@@ -5,15 +5,17 @@ describe("Given 2^n teams, it returns paired matches", () => {
   let teams = ["a", "b", "c", "d", "e", "f", "g", "h"];
   let incorrect = ["a", "b", "c"];
 
-  let matches = matchMaker(teams);
-  let noMatches = matchMaker(incorrect);
+  beforeEach(() => {
+    let matches = matchMaker(teams);
+    let noMatches = matchMaker(incorrect);
 
-  it("does not create matches for number fo teams different than 2^n", () => {
-    expect(noMatches.length).toEqual(0);
-  });
+    it("does not create matches for number fo teams different than 2^n", () => {
+      expect(noMatches.length).toEqual(0);
+    });
 
-  it("creates a matches array of half the length of teams", () => {
-    expect(matches.length).toEqual(teams.length / 2);
+    it("creates a matches array of half the length of teams", () => {
+      expect(matches.length).toEqual(teams.length / 2);
+    });
   });
 
   describe("isPowerOfTwo validates numbers correctly", () => {
