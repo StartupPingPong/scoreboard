19c68b630d5dbc8e3ba828be43d8786a1440b1c9
joseph_ctu <sephxd1234@gmail.com>
2018-01-21 03:42:17 +0100

    test: matchMaker ignores number of teams different than 2^n

--- client/src/utils/index.test.js
+++ client/src/utils/index.test.js
@@ -3,7 +3,15 @@ import { matchMaker } from "./";
 
 describe("Given 2^n teams, it returns paired matches", () => {
   let teams = ["a", "b", "c", "d", "e", "f", "g", "h"];
+  let incorrect = ["a", "b", "c"];
+
   let matches = matchMaker(teams);
+  let noMatches = matchMaker(incorrect);
+
+  it("does not create matches for number fo teams different than 2^n", () => {
+    expect(noMatches).toEqual([]);
+  });
+
   it("creates a matches array of half the length of teams", () => {
     expect(matches.length).toEqual(teams.length / 2);
   });
