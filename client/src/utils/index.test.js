b846199d83701bdb1eb8bf1c7b218e1dadbb4439
joseph_ctu <sephxd1234@gmail.com>
2018-01-22 07:43:20 +0100

    refactor: group test of support functions to their own scope

--- client/src/utils/index.test.js
+++ client/src/utils/index.test.js
@@ -17,14 +17,14 @@ describe("Given 2^n teams, it returns paired matches", () => {
       expect(matches.length).toEqual(teams.length / 2);
     });
   });
+});
 
-  describe("support functions work correctly", () => {
-    describe("isPowerOfTwo validates numbers correctly", () => {
-      let testArr = [0, 1, 2, 3, 4, 7, 8, 9, 10, 256, -16, "a"];
+describe("support functions work correctly", () => {
+  describe("isPowerOfTwo validates numbers correctly", () => {
+    let testArr = [0, 1, 2, 3, 4, 7, 8, 9, 10, 256, -16, "a"];
 
-      it("returns the expected answer", () => {
-        expect(testArr.map(e => isPowerOfTwo(e))).toMatchSnapshot();
-      });
+    it("returns the expected answer", () => {
+      expect(testArr.map(e => isPowerOfTwo(e))).toMatchSnapshot();
     });
   });
 });
